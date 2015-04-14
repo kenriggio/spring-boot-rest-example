d5d12b991d4398e9e24c98c81fa4e1a88dc5c9b2
Riggio <kennethr@kennethr-mbr.telenav.com>
2015-04-13 17:26:43 -0700

    Added more Swagger based documentation

--- src/main/java/com/thinknear/attribution/web/model/UserLocation.java
+++ src/main/java/com/thinknear/attribution/web/model/UserLocation.java
@@ -1,19 +1,26 @@
 package com.thinknear.attribution.web.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
 import org.springframework.hateoas.ResourceSupport;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@ApiModel(value = "Location of a User")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserLocation extends ResourceSupport {
 
+    @ApiModelProperty(value = "The surrogate key for this record", required=false)
     @JsonProperty("id")
     private String userLocationId; // using userLocationId instead of Id, since ResourceSupport using id for link.
+    @ApiModelProperty(value = "The user id for this event", required=false)
     @JsonProperty("userId")
     private String userId;
+    @ApiModelProperty(value = "latitude of the user for this event", required=false)
     @JsonProperty("lat")
     private String lat;
+    @ApiModelProperty(value = "longitude of the user for this event", required=false)
     @JsonProperty("lon")
     private String lon;
 
--- src/main/java/com/thinknear/attribution/web/service/impl/UserLocationServiceImpl.java
+++ src/main/java/com/thinknear/attribution/web/service/impl/UserLocationServiceImpl.java
@@ -5,7 +5,6 @@ import com.thinknear.attribution.annotation.Throwit;
 import com.thinknear.attribution.web.service.UserLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import com.thinknear.attribution.web.model.UserLocation;
 import com.thinknear.attribution.web.dao.UserLocationDao;
 import org.slf4j.Logger;
@@ -17,12 +16,6 @@ public class UserLocationServiceImpl implements UserLocationService {
     static final Logger logger = LoggerFactory.getLogger(UserLocationServiceImpl.class);
 
     private UserLocationDao userLocationDao;
-
-    private static RestTemplate restTemplate;
-
-    static {
-        restTemplate = new RestTemplate();
-    }
 
     @Autowired
     public UserLocationServiceImpl(UserLocationDao userLocationDao) {
